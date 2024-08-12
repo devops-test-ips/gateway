efae6c19bea5a647aad01331d864c666b463997b
ozen-hub <info.ozenhub@gmail.com>
2024-08-12 22:08:41 +0530

    init

--- src/main/java/com/devops/gateway/security/SecurityConfiguration.java
+++ src/main/java/com/devops/gateway/security/SecurityConfiguration.java
@@ -2,10 +2,16 @@ package com.devops.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -23,4 +29,17 @@ public class SecurityConfiguration {
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
         return serverHttpSecurity.build();
     }
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration c = new CorsConfiguration();
+        c.setAllowedOrigins(List.of("*"));
+        c.setAllowedMethods(List.of("PUT", "GET", "POST", "DELETE", "OPTION", "PATCH"));
+        c.setAllowedHeaders(List.of("*"));
+        c.setAllowCredentials(false);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",c);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+    }
+
 }
